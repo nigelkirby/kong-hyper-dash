de0b8c539425c7e5370f75a3bf877e69cdfa96c2
Nigel Kirby <nigel.kirby@gmail.com>
2018-07-28 14:31:35 -0400

    created updated added

--- src/components/ServiceSpotlight.jsx
+++ src/components/ServiceSpotlight.jsx
@@ -6,19 +6,22 @@ const parseToUrl = ({
 
 const makeLink = url => <a href={url}>{url}</a>
 
-export default ({ service }) => (
+export default ({ service }) =>
   service && (
-      <div>
-        <h2>{service.name || service.id}</h2>
-        <p class="small-text">
-          <em>{service.id}</em>
-          <br />
-          URL: {makeLink(parseToUrl(service))}
-        </p>
-        <h3>Routes</h3>
-        <p>{service.routes.length > 0 ? <p /> : <span>No Routes</span>}</p>
-        <h3>Plugins</h3>
-        <p>{service.plugins.length > 0 ? <p /> : <span>No Plugins</span>}</p>
-      </div>
+    <div>
+      <h2>{service.name || service.id}</h2>
+      <p class="small-text">
+        <em>{service.id}</em>
+        <br />
+        URL: {makeLink(parseToUrl(service))}
+        <br />
+        Created: {`${new Date(service.created_at * 1000)}`}
+        <br />
+        Updated: {`${new Date(service.updated_at * 1000)}`}
+      </p>
+      <h3>Routes</h3>
+      <p>{service.routes.length > 0 ? <p /> : <span>No Routes</span>}</p>
+      <h3>Plugins</h3>
+      <p>{service.plugins.length > 0 ? <p /> : <span>No Plugins</span>}</p>
+    </div>
   )
-)
