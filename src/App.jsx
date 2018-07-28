8d33cba83d11659daf7fb8090900a452dc5c7de2
Nigel Kirby <nigel.kirby@gmail.com>
2018-07-27 20:32:22 -0400

    initial commit, hyperapp with minicss

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { h } from 'hyperapp'
+import { Header } from './components'
+
+/* eslint-disable-next-line no-unused-vars */
+export default (state, actions) => (
+    <div>
+      <Header />
+    </div>
+)
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+import { h } from 'hyperapp'
+
+export default () => (
+  <header>
+    <span class='logo'>Kong Dashboard</span>
+  </header>
+)
